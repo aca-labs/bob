1c6c4ec4b3debc7b1ff3ea5c93b3c30c573ac3fa
Kim Burgess <kim@acaprojects.com>
2019-08-19 21:24:18 +1000

    use shards to extract version

--- src/bob/version.cr
+++ src/bob/version.cr
@@ -1,9 +1,3 @@
 module Bob
-  VERSION = {{
-    read_file("#{__DIR__}/../../shard.yml")
-      .lines
-      .map(&.strip)
-      .find(&.starts_with? "version")
-      .gsub(/version\: ?/, "")
-  }}
+  VERSION = {{ `shards version`.stringify }}
 end
