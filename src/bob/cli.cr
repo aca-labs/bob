b1b12b24956d78355477fa51e0ac1f54fc6a4d1e
Kim Burgess <kim@acaprojects.com>
2019-08-19 21:27:31 +1000

    add param for specifiying built image tag

--- src/bob/cli.cr
+++ src/bob/cli.cr
@@ -35,6 +35,9 @@ class Bob::Cli
       exit
     end
 
+    name = nil
+    opts.on("-t", "--tag", "The name to tag the built images with.") { |x| name = x }
+
     opts.invalid_option do |flag|
       Terminimal.exit_with_error "unkown option '#{flag}'", Errno::EINVAL
     end
