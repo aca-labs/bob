9526fe9b2b94c44b8ba872766fbfd351223358fb
Caspian Baska <caspianbaska@gmail.com>
2019-09-04 14:40:13 +1000

    alter logging

--- src/bob/builder.cr
+++ src/bob/builder.cr
@@ -15,14 +15,16 @@ class Bob::Builder
 
   # Runs a build based on the current path contents.
   def build
-    puts "building #{path}"
+    puts "#{self}: action=build path=#{path} name=#{name}"
     docker.images.build path, t: name
   end
 
   # Start watching the path and automatically build when a new git commit is made, or when switching
   # to a new branch.
   def watch : Nil
+    puts "#{self.inspect}: action=watch"
     @watcher ||= Inotify.watch "#{path}/.git/logs/HEAD" do |event|
+      puts "#{self}: action=watch event=#{event}"
       next unless event.type == Inotify::Event::Type::MODIFY
 
       # FIXME: implement a file reader that tails this
@@ -35,7 +37,7 @@ class Bob::Builder
 
       details = entry.split("\t")[1].chomp
 
-      puts "git repo change detected (#{details}), rebuilding"
+      puts "#{self}: git repo change detected (#{details}), rebuilding"
 
       begin
         build
